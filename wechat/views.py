11352a6f572c4183cc25ea34f14368b8c523e204
yancong <yc1191834709@163.com>
2018-02-13 10:41:28 +0800

    add encrypt

--- wechat/views.py
+++ wechat/views.py
@@ -1,35 +1,68 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse  
-from django.views.decorators.csrf import csrf_exempt  
-from wechatpy import parse_message, create_reply  
-from wechatpy.exceptions import InvalidSignatureException  
-from wechatpy.utils import check_signature  
-  
+from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from wechatpy import parse_message, create_reply
+from wechatpy.utils import check_signature
+from wechatpy.exceptions import (
+    InvalidSignatureException,
+    InvalidAppIdException,
+)
+
 WECHAT_TOKEN = 'abcd'
-@csrf_exempt  
-def wechat(request): 
-    if request.method == 'GET': 
-        signature = request.GET.get('signature', '')
-        timestamp = request.GET.get('timestamp', '')
-        nonce = request.GET.get('nonce', '')
+WECHAT_AES_KEY = ''
+WECHAT_APPID = ''
+
+@csrf_exempt
+def wechat(request):
+
+    signature = request.GET.get('signature', '')
+    timestamp = request.GET.get('timestamp', '')
+    nonce = request.GET.get('nonce', '')
+    encrypt_type = request.GET.get('encrypt_type', 'raw')
+    msg_signature = request.GET.get('msg_signature', '')
+
+    try:
+        check_signature(WECHAT_TOKEN, signature, timestamp, nonce)
+    except InvalidSignatureException:
+        return HttpResponse(status=403)
+    if request.method == 'GET':
         echo_str = request.GET.get('echostr', '')
-        try:
-            check_signature(WECHAT_TOKEN, signature, timestamp, nonce)  
-        except InvalidSignatureException:  
-            echo_str = 'error'  
-        response = HttpResponse(echo_str, content_type="text/plain")  
-        return response  
+        return HttpResponse(echo_str, content_type="text/plain")
     elif request.method == 'POST':  
-        msg = parse_message(request.body)  
-        if msg.type == 'text':  
-            reply = create_reply('这是条文字消息', msg)  
-        elif msg.type == 'image':  
-            reply = create_reply('这是条图片消息', msg)  
-        elif msg.type == 'voice':  
-            reply = create_reply('这是条语音消息', msg)  
-        else:  
-            reply = create_reply('这是条其他类型消息', msg)  
-        response = HttpResponse(reply.render(), content_type="application/xml")  
-        return response  
+		if encrypt_type == 'raw':
+            msg = parse_message(request.body)
+            if msg.type == 'text':
+                reply = create_reply('这是条文字消息', msg)
+            elif msg.type == 'image':
+                reply = create_reply('这是条图片消息', msg)
+            elif msg.type == 'voice':
+                reply = create_reply('这是条语音消息', msg)
+            else:
+                reply = create_reply('这是条其他类型消息', msg)
+            return HttpResponse(reply.render(), content_type="application/xml")
+        else:
+            from wechatpy.crypto import WeChatCrypto
+
+            crypto = WeChatCrypto(WECHAT_TOKEN, WECHAT_AES_KEY, WECHAT_APPID)
+            try:
+                msg = crypto.decrypt_message(
+                    request.body,
+                    msg_signature,
+                    timestamp,
+                    nonce
+                )
+            except (InvalidSignatureException, InvalidAppIdException):
+                return HttpResponse(status=403)
+            else:
+                msg = parse_message(msg)
+                if msg.type == 'text':
+                    reply = create_reply('这是条文字消息', msg)
+                elif msg.type == 'image':
+                    reply = create_reply('这是条图片消息', msg)
+                elif msg.type == 'voice':
+                    reply = create_reply('这是条语音消息', msg)
+                else:
+                    reply = create_reply('这是条其他类型消息', msg)
+                return HttpResponse(crypto.encrypt_message(reply.render(), nonce, timestamp), content_type="application/xml")
     else:  
         logger.info('--------------------------------')  
